69957e08df17676f58731a123c6f5fcdada28fe5
Venkatesh <venkypoluri898@gmail.com>
2024-09-03 22:50:55 +0530

    Best Time to Buy and Sell Stocks 2

---
+++ src/main/java/org/example/LeetCodeDSA/BestTimeToBuyAndSellStock2.java
@@ -0,0 +1,20 @@
+package org.example.LeetCodeDSA;
+
+public class BestTimeToBuyAndSellStock2 {
+    public static void main(String[] args) {
+        int prices[]={7,1,5,3,6,4};
+        int result=maxProfit(prices);
+        System.out.println(result);
+
+    }
+    public static int maxProfit(int[] prices) {
+        int profit=0;
+        for(int i=1;i<prices.length;i++)
+        {
+            if(prices[i]>prices[i-1])
+                profit+=prices[i]-prices[i-1];
+        }
+        return profit;
+
+    }
+}
