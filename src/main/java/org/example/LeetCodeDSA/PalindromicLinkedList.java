5e5b74ff73d113ee683b1f2b5397916d0efd6db3
Venkatesh <venkypoluri898@gmail.com>
2024-09-30 21:43:25 +0530

    Committing java code for palindromic linked list.

--- src/main/java/org/example/LeetCodeDSA/PalindromicLinkedList.java
+++ src/main/java/org/example/LeetCodeDSA/PalindromicLinkedList.java
@@ -4,7 +4,7 @@ public class PalindromicLinkedList {
     ListNode head;
     static class ListNode {
         int val;
-        ListNode next;
+        ListNode  next;
         ListNode() {}
         ListNode(int val) { this.val = val; }
         ListNode(int val, ListNode next) { this.val = val; this.next = next; }
