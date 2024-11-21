2b231db761d8e3dfe7fdfe1cc8c7e3b2509be1ca
Venkatesh <venkypoluri898@gmail.com>
2024-11-21 15:53:40 +0530

    Lowest Common Ancestors of a binary Tree using DFS.

---
+++ src/main/java/org/example/LeetCodeDSA/tree/LowestCommonAncestorsofABT.java
@@ -0,0 +1,21 @@
+package org.example.LeetCodeDSA.tree;
+
+public class LowestCommonAncestorsofABT {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root==null)
+        {
+            return null;
+        }
+        if(root==p||root==q)
+        {
+            return root;
+        }
+        TreeNode left=lowestCommonAncestor(root.left,p,q);
+        TreeNode right=lowestCommonAncestor(root.right,p,q);
+        if(left!=null&&right!=null)
+        {
+            return root;
+        }
+        return left==null?right:left;
+    }
+}
